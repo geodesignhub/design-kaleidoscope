63231beb0df6d085605e011fe804978fd47101a7
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-10-04 15:11:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Design Kaleidoscope
+
+This is small plugin to help with visualizing a Geodesignhub Design synthesis. It uses the [Geodesignhub API](http://www.geodesignhub.com/api/). The tool downloads the design synthesis to vary transparency of the systems based on your preferences. It can be useful to understand how your priorities performs spatially and stimulate discussion in your team.
+
+## Adding to your project
+
+This plugin can be added to your project using the Administration panel or at the time of creating a project.
+
+## Screenshot
+
+Edit the polygon
+
+![Ui diagrams](img/../screenshot-base.jpg)
---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var req = require('request');
+var async = require('async');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
+var csrf = require('csurf');
+
+var app = express();
+app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/views'));
+app.use('/assets', express.static('static'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
+
+app.get('/', function (request, response) {
+    var opts = {};
+
+    if (request.query.apitoken && request.query.projectid && request.query.cteamid && request.query.synthesisid) {
+        // var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+        var baseurl = 'http://local.test:8000/api/v1/projects/';
+        const apikey = request.query.apitoken;
+        const projectid = request.query.projectid;
+        const cred = "Token " + apikey;
+        const systems_url = baseurl + projectid + '/systems/';
+        const bounds_url = baseurl + projectid + '/bounds/';
+        const cteamid = request.query.cteamid;
+        const synthesisid = request.query.synthesisid;
+        const synprojectsurl = baseurl + projectid + '/cteams/' + cteamid + '/' + synthesisid + '/';
+        
+        let URLS = [systems_url, synprojectsurl, bounds_url]
+
+        async.map(URLS, function (url, done) {
+            req({
+                url: url,
+                headers: {
+                    "Authorization": cred,
+                    "Content-Type": "application/json"
+                }
+            }, function (err, response, body) {
+                if (err || response.statusCode !== 200) {
+                    return done(err || new Error());
+                }
+                return done(null, JSON.parse(body));
+            });
+        }, function (err, results) {
+            if (err) return response.sendStatus(500);
+            let opts = {
+                "data": results,
+            
+            };
+            response.render('kaledioscope', opts);
+        });
+
+    } else {
+        response.status(400);
+        response.send('Please pass all the valid parameters.');
+    }
+
+});
+
+app.listen(process.env.PORT || 5001);
